9a5675d8985edb2b6881bdb8b46005207f1a00c7
Mark Taylor <moderngeekish@gmail.com>
2017-09-03 22:38:42 +0100

    Set the time via state
    Currently just uses a set interval in the component, will be updated to use events later

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Clock from "./components/clock/index";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Clock date={new Date()}/>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Clock date={this.state.date}/>
+            </div>
+        );
+    }
 }
 
 export default App;
