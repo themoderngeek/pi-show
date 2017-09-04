e4efbcbe704f3c4cc04d2e3908323986f1e2b49d
Mark Taylor <themoderngeek@users.noreply.github.com>
2017-09-04 22:44:40 +0100

    Clock refactor (#5)
    * Added the date to the clock component
    
    * Added package local
    
    * Updated docker file
    
    * Added a container around the the clock and tidied up the Clock styles
    
    * Default to analog
    
    * Refactor of the clock component

--- src/components/clock/index.jsx
+++ src/components/clock/index.jsx
@@ -1,46 +1,39 @@
 import React, {Component} from 'react';
 import './Clock.css';
 
+const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const timeOptions = { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+let secondsStyle, minutesStyle, hoursStyle;
+
 export default class Clock extends Component {
 
-
-    constructor(props) {
-        super(props);
+    setAnalogStyles() {
+        let hoursTransform = 30;
+        if(this.props.date.getMinutes() > 30) {
+            hoursTransform = 31;
+        }
+        secondsStyle = {transform: ['rotateZ(' + this.props.date.getSeconds() * 6 + 'deg)']};
+        minutesStyle = {transform: ['rotateZ(' + this.props.date.getMinutes() * 6 + 'deg)']};
+        hoursStyle = {transform: ['rotateZ(' + this.props.date.getHours() * hoursTransform + 'deg)']};
     }
 
     render() {
-        const time = this.props.date.toLocaleTimeString([], {
-            hour12: false,
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit'
-        });
-
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-
-        let secondsStyle, minutesStyle, hoursStyle;
+        const time = this.props.date.toLocaleTimeString([], timeOptions);
+        const date = this.props.date.toLocaleDateString("en-Uk", dateOptions);
 
         if(this.props.type === "analog") {
-            let hoursTransform = 30;
-            if(this.props.date.getMinutes() > 30) {
-                hoursTransform = 31;
-            }
-            secondsStyle = {transform: ['rotateZ(' + this.props.date.getSeconds() * 6 + 'deg)']};
-            minutesStyle = {transform: ['rotateZ(' + this.props.date.getMinutes() * 6 + 'deg)']};
-            hoursStyle = {transform: ['rotateZ(' + this.props.date.getHours() * hoursTransform + 'deg)']};
+            this.setAnalogStyles();
         }
 
         return (
-
             <div className={this.props.type}>
                 <div className="clock show">
-                    <div className="hours" style={hoursStyle}>{time[0] + time[1]}</div>
-                    <div className="minutes" style={minutesStyle}>{time[3] + time[4]}</div>
-                    <div className="seconds" style={secondsStyle}>{time[6] + time[7]}</div>
+                    <div className="hours" style={ hoursStyle }>{ time[0] + time[1] }</div>
+                    <div className="minutes" style={ minutesStyle }>{ time[3] + time[4] }</div>
+                    <div className="seconds" style={ secondsStyle }>{ time[6] + time[7] }</div>
                 </div>
                 <div className="DateView">
-                    {this.props.date.toLocaleDateString("en-Uk", options)}
+                    { date }
                 </div>
             </div>
         );
